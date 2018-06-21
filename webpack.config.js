0576ba05dacb622774993c7efdc1730166b4c4f2
Nick Chapman <nchapman@gmail.com>
2018-06-20 20:05:01 -0700

    Switched from sass to less to resolve security issue.

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,9 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader' // compiles Sass to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader'
         ]
       }
     ]
